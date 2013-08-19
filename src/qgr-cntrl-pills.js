b11983c273a40507ed9e46738182e2ee618e862f
sprin <sprin>
2013-08-19 13:29:15 -0700

    Fix several bugs under IE8

--- src/qgr-cntrl-pills.js
+++ src/qgr-cntrl-pills.js
@@ -46,11 +46,14 @@ define(function (require) {
       var t = this;
 
       var choices = _.map(this.choice_model.get('choices'), function(choice) {
+        // Handle null specially for IE8.
+        var choice_val = choice ? choice.val : '';
+
         // Flag the selected choice.
-        if (choice.val === t.choice_model.get('choice_val')) {
+        if (choice_val === t.choice_model.get('choice_val')) {
           return _.extend({}, choice, {selected: true});
         }
-        return choice;
+        return _.extend({}, choice, {selected: false});
       });
 
       var render_content = this.tmpl({
@@ -63,7 +66,7 @@ define(function (require) {
     },
 
     set_choice: function(e) {
-      this.choice_model.set('choice_val', e.currentTarget.dataset.choice);
+      this.choice_model.set('choice_val', $(e.currentTarget).data('choice'));
       this.render();
     },
 
